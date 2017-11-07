eda83aecdd9ed2b8f3efe8f9704c03b6bfa7521f
Canta Coop <cantacoop@gmail.com>
2017-11-07 22:23:09 +0700

    Create WeatherProvider class

---
+++ app/src/main/java/com/example/android/sunshine/data/WeatherProvider.java
@@ -0,0 +1,152 @@
+package com.example.android.sunshine.data;
+
+
+import android.annotation.TargetApi;
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+/**
+ * This class serves as the ContentProvider for all of Sunshine's data. This class allows us to
+ * bulkInsert data, query data, and delete data.
+ * <p>
+ * Although ContentProvider implementation requires the implementation of additional methods to
+ * perform single inserts, updates, and the ability to get the type of the data from a URI.
+ * However, here, they are not implemented for the sake of brevity and simplicity. If you would
+ * like, you may implement them on your own. However, we are not going to be teaching how to do
+ * so in this course.
+ */
+public class WeatherProvider extends ContentProvider {
+
+    // Declare, but don't instantiate a WeatherDbHelper object called mOpenHelper
+    private WeatherDbHelper mOpenHelper;
+
+    /**
+     * In onCreate, we initialize our content provider on startup. This method is called for all
+     * registered content providers on the application main thread at application launch time.
+     * It must not perform lengthy operations, or application startup will be delayed.
+     *
+     * Nontrivial initialization (such as opening, upgrading, and scanning
+     * databases) should be deferred until the content provider is used (via {@link #query},
+     * {@link #bulkInsert(Uri, ContentValues[])}, etc).
+     *
+     * Deferred initialization keeps application startup fast, avoids unnecessary work if the
+     * provider turns out not to be needed, and stops database errors (such as a full disk) from
+     * halting application launch.
+     *
+     * @return true if the provider was successfully loaded, false otherwise
+     */
+    @Override
+    public boolean onCreate() {
+        /*
+         * As noted in the comment above, onCreate is run on the main thread, so performing any
+         * lengthy operations will cause lag in your app. Since WeatherDbHelper's constructor is
+         * very lightweight, we are safe to perform that initialization here.
+         */
+        mOpenHelper = new WeatherDbHelper(getContext());
+
+        return true;
+    }
+
+    /**
+     * Handles requests to insert a set of new rows. In Sunshine, we are only going to be
+     * inserting multiple rows of data at a time from a weather forecast. There is no use case
+     * for inserting a single row of data into our ContentProvider, and so we are only going to
+     * implement bulkInsert. In a normal ContentProvider's implementation, you will probably want
+     * to provide proper functionality for the insert method as well.
+     *
+     * @param uri    The content:// URI of the insertion request.
+     * @param values An array of sets of column_name/value pairs to add to the database.
+     *               This must not be {@code null}.
+     *
+     * @return The number of values that were inserted.
+     */
+    @Override
+    public int bulkInsert(@NonNull Uri uri, @NonNull ContentValues[] values) {
+        throw new RuntimeException("Student, you need to implement the bulkInsert mehtod!");
+    }
+
+    /**
+     * Handles query requests from clients. We will use this method in Sunshine to query for all
+     * of our weather data as well as to query for the weather on a particular day.
+     *
+     * @param uri           The URI to query
+     * @param projection    The list of columns to put into the cursor. If null, all columns are
+     *                      included.
+     * @param selection     A selection criteria to apply when filtering rows. If null, then all
+     *                      rows are included.
+     * @param selectionArgs You may include ?s in selection, which will be replaced by
+     *                      the values from selectionArgs, in order that they appear in the
+     *                      selection.
+     * @param sortOrder     How the rows in the cursor should be sorted.
+     * @return A Cursor containing the results of the query. In our implementation,
+     */
+    @Override
+    public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
+        return null;
+    }
+
+    /**
+     * In Sunshine, we aren't going to do anything with this method. However, we are required to
+     * override it as WeatherProvider extends ContentProvider and getType is an abstract method in
+     * ContentProvider. Normally, this method handles requests for the MIME type of the data at the
+     * given URI. For example, if your app provided images at a particular URI, then you would
+     * return an image URI from this method.
+     *
+     * @param uri the URI to query.
+     * @return nothing in Sunshine, but normally a MIME type string, or null if there is no type.
+     */
+    @Override
+    public String getType(@NonNull Uri uri) {
+        return null;
+    }
+
+    /**
+     * In Sunshine, we aren't going to do anything with this method. However, we are required to
+     * override it as WeatherProvider extends ContentProvider and insert is an abstract method in
+     * ContentProvider. Rather than the single insert method, we are only going to implement
+     * {@link WeatherProvider#bulkInsert}.
+     *
+     * @param uri    The URI of the insertion request. This must not be null.
+     * @param values A set of column_name/value pairs to add to the database.
+     *               This must not be null
+     * @return nothing in Sunshine, but normally the URI for the newly inserted item.
+     */
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
+        return null;
+    }
+
+    /**
+     * Deletes data at a given URI with optional arguments for more fine tuned deletions.
+     *
+     * @param uri           The full URI to query
+     * @param selection     An optional restriction to apply to rows when deleting.
+     * @param selectionArgs Used in conjunction with the selection statement
+     * @return The number of rows deleted
+     */
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+
+    /**
+     * You do not need to call this method. This is a method specifically to assist the testing
+     * framework in running smoothly. You can read more at:
+     * http://developer.android.com/reference/android/content/ContentProvider.html#shutdown()
+     */
+    @Override
+    @TargetApi(11)
+    public void shutdown() {
+        mOpenHelper.close();
+        super.shutdown();
+    }
+}
